a21d30f5a5886be35778b72bd2ce4db435d512b0
Sjankaczar <capriodemaestro@gmail.com>
2025-03-11 22:00:47 +0700

    Update tutorial.py

--- src/tutorial.py
+++ src/tutorial.py
@@ -1,13 +1,14 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import src.graph as g
 
 list_penerbangan = [("Indonesia", "Singapura"), ("Indonesia", "Jepang"), ("Indonesia", "Uni Emirat Arab"), ("Singapura", "Uni Emirat Arab"), ("Singapura", "Inggris"), 
                     ("Singapura", "Amerika"), ("Jepang", "Singapura"), ("Jepang", "Amerika"), ("Jepang", "Uni Emirat Arab"), ("Jepang", "Inggris"), ("Uni Emirat Arab", "Amerika"),
                     ("Uni Emirat Arab", "Inggris"), ("Amerika", "Inggris")]
 
-J = create_graph(list_penerbangan)
-get_degree(J, "Singapura")
-dfs_traversal(J, "Indonesia")
-bfs_traversal(J, "Indonesia")
-find_shortest_path(J, "Indonesia", "Amerika")
-visualize_graph(J)
+J = g.create_graph(list_penerbangan)
+g.get_degree(J, "Singapura")
+g.dfs_traversal(J, "Indonesia")
+g.bfs_traversal(J, "Indonesia")
+g.find_shortest_path(J, "Indonesia", "Amerika")
+g.visualize_graph(J)
